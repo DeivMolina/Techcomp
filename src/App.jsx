b25abba7e883d0887172385dadb00066ff412929
DeivMolina <100443503+DeivMolina@users.noreply.github.com>
2024-02-19 18:06:24 -0600

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import styled from 'styled-components'; // Agrega esta línea
+
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
@@ -8,17 +10,25 @@ import Portfolio from './components/portfolio/Portfolio'
 import Testimonial from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import { NieveComponente } from './components/NieveComponente'
+
+const MainContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
 
 const App = () => {
   return (
-    <>
-        <Header />
-        <Nav />
-        <Portfolio />
-        <Contact />
-        <Footer />
-    </>
-  )
-}
+    <MainContainer>
+      <NieveComponente />
+      <Header />
+      <Nav />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </MainContainer>
+  );
+};
 
 export default App
