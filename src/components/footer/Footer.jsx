7db0c03e5921bdde72147b724a6fdb6da1f49fa2
Deivid Molina <davidglezm2015@gmail.com>
2024-02-21 11:10:13 -0600

    debug

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,8 @@ import {FiTwitter} from 'react-icons/fi'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'> TechComp</a>
+      <a href="#" className='footer__logo' style={{ pointerEvents: 'none' }}>TechComp</a>
+
 
       <ul className='permalinks'>
         <li> <a href="https://techcomp-instruments.com/?lang=es" target="_blank">Visita nuestro Sitio</a> </li>
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import CV from '../../assets/cv.pdf'
-import {BsWhatsapp} from 'react-icons/bs'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="https://api.whatsapp.com//send?phone=5570081678" target="_blank" className='btn'><BsWhatsapp/> Whatsapp</a>
+        <a href="https://api.whatsapp.com//send?phone=5570081678" target="_blank" className='btn'> Whatsapp</a>
         <a className='btn btn-primary' href='tel:5570081678'>Llámanos</a>
     </div>
   )
