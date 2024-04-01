13735b8a0dd6703019961357e632bdc0efe98415
DeivMolina <100443503+DeivMolina@users.noreply.github.com>
2024-04-01 14:38:26 -0600

    productos

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
+import Product from './components/product/Product';
 import Testimonial from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
@@ -25,6 +26,7 @@ const App = () => {
       <NieveComponente />
       <Nav />
       <Portfolio />
+      <Product />
       <Contact />
       <Footer />
     </MainContainer>
